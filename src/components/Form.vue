aeab47b42f4f8fc6c095bc44983b6bcf683b2f79
周鑫 <zhouxin@zhufaner.com>
2022-08-30 15:35:11 +0800

    fix:挂载和销毁emiiter

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,19 +6,20 @@
 		<input v-model="formData.prefix" type="text"
 			class=" mb-6 w-64 leading-8 h-8 bg-white border-gray-500 border text-center" placeholder="简写" />
 
-		<textarea ref="textarea" v-model="formData.body" class=" bg-white w-3/4 border height mb-6" rows="25"></textarea>
+		<textarea ref="textarea" v-model="formData.body" class=" bg-white w-3/4 border height mb-6 p-3"
+			rows="25"></textarea>
 		<button class="p-2 flex items-center justify-center rounded-md bg-blue-500 text-white mb-6 appearance-none"
-			@click.prevent="clearFormData">清空</button>
+			@click.stop="clearFormData">清空</button>
 		<button class="p-2 flex items-center justify-center rounded-md bg-blue-500 text-white mb-6 appearance-none"
-			@click.prevent="emitcopy">复制配置</button>
+			@click.stop="emitcopy">复制配置</button>
 
 
-			<div class="text-gray-600">ctrl+i 可快速插入占位光标 如${0:example}</div>
+		<div class="text-gray-600">ctrl+i 可快速插入占位光标 如${0:example}</div>
 	</div>
 </template>
 
 <script  setup lang="ts">
-import { reactive, watch, ref, nextTick } from "vue";
+import { reactive, watch, ref, nextTick, onUnmounted } from "vue";
 import { emitter } from "../emitt"
 import { Icontent, insertAtCursor } from "../utils"
 let textarea = ref(null)
@@ -30,19 +31,23 @@ const formData = reactive<Icontent>({
 watch(formData, (updateData: Icontent) => {
 	emitter.emit('change', updateData)
 })
+onUnmounted(() => {
+	emitter.off('*')
+})
 let number = 1;
 nextTick(() => {
 	let element: HTMLTextAreaElement = textarea.value!
 	element.addEventListener('keypress', function (e: KeyboardEvent) {
 
 		if (e.key === 'i') {
-			insertAtCursor(element,`\${${number}:example}`)
+			insertAtCursor(element, `\${${number}:example}`)
 			number++;
 			formData.body = element.value;
 		}
 	})
 })
 function emitcopy() {
+	console.log('copy emit');
 	emitter.emit('copy')
 }
 function clearFormData() {
